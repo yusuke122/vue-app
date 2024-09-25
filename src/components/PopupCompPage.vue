02a54fe9fba4037ba9381b788a38a9d89f3d1812
yusuke122 <youfufumu@gmail.com>
2024-09-25 22:12:15 +0900

    modalにはbootstrap.min.js popper.min.jsが必要

--- src/components/PopupCompPage.vue
+++ src/components/PopupCompPage.vue
@@ -1,20 +1,20 @@
 <template>
     <div>
         <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
   Launch demo modal
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="staticBackdrop" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="staticBackdrop">Modal title</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+        {{ content }}
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -25,3 +25,19 @@
 </div>
     </div>
 </template>
+<script setup>
+const content = "データを渡す"
+</script>
+<style scoped>
+.modal-content{
+    background-color: #333333;
+    padding:-1rem;
+    color:#aaaaaa
+}
+.modal-header{
+    border:1rem solid #333333
+}
+.modal-footer{
+    border:1rem solid #333333
+}
+</style>
