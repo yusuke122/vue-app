dfe2755b0cb8af7bcbaeabe797d6f61566fb304a
yusuke122 <86652008+yusuke122@users.noreply.github.com>
2025-05-13 06:54:01 +0900

    HomeView.vue を更新

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -185,7 +185,7 @@ onMounted(() => {
 </script>
 <template>
   <div id="map" class="map"></div>
-  <！--div id="popup" class="ol-popup">
+  <!--div id="popup" class="ol-popup">
     <a href="#" id="popup-closer" class="ol-popup-closer"></a>
     <div id="popup-content"></div>
   </div-->
