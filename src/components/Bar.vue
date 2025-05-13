714a402f07fde763853fdeb4aa32271cd6d8df69
yusuke122 <86652008+yusuke122@users.noreply.github.com>
2025-05-14 07:17:57 +0900

    Bar.vue を作成

---
+++ src/components/Bar.vue
@@ -0,0 +1,15 @@
+<!-- BarChart.vue -->
+<template>
+  <Bar :data="chartData" />
+</template>
+
+<script setup>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(BarElement, CategoryScale, LinearScale)
+
+const props = defineProps({
+  chartData: Object
+})
+</script>
